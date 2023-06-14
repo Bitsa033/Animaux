418a7514358bc6403597a1320827f431f36a623d
Bitsapascal <bitapascal033@gmail.com>
2023-06-14 21:37:46 +0100

    on essaie de travailler avec les services test n°1

--- src/app/animaux/animaux.component.ts
+++ src/app/animaux/animaux.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./animaux.component.css']
 })
 export class AnimauxComponent {
-
+  animaux=[
+    {id:1,Nom:'Grislain',sexe:'Femele',qte:31},
+    {id:2,Nom:'Raoul',sexe:'Male',qte:55},
+    {id:3,Nom:'Robert',sexe:'Femele',qte:189},
+    {id:4,Nom:'Flamme',sexe:'Male',qte:1200},
+    {id:4,Nom:'Roky',sexe:'Femele',qte:1200},
+    
+  ]
 }
