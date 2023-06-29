db5552820d34a6e2b9179d311e25dae538df256a
Bitsapascal <bitapascal033@gmail.com>
2023-06-29 01:03:46 +0100

    on a ajouté le système de pagination

--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { UtilsService } from '../utils.service';
+import { ProduitsService } from '../service/produits.service';
 
 
 @Component({
@@ -11,7 +12,10 @@ import { UtilsService } from '../utils.service';
 })
 export class ProduitsComponent implements OnInit {
   
-  produits:any
+  produits!:Array<ProduitsService>
+  curentPage:number=0
+  nbElements:number=3
+  totalPage:number=0
   erreur:any
   reussite:any
 
@@ -20,7 +24,7 @@ export class ProduitsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getProduits();
+    this.getProduitsWithPagination();
   }
   
   getProduits(){
@@ -36,14 +40,42 @@ export class ProduitsComponent implements OnInit {
     
   }
 
-  deleteData(id:any){
-    this.service.deleteProduct(id).subscribe((res)=>{
+  goToPage(i:number){
+    this.curentPage=i
+    this.getProduitsWithPagination()
+  }
+
+  getProduitsWithPagination(){
+    this.service.getAllProducts().subscribe((response:any)=>{
+      this.produits=response.data
+      let index= this.curentPage*this.nbElements
+      this.totalPage = ~~(this.produits.length / this.nbElements)
+      if (this.produits.length % this.nbElements !=0) {
+        this.totalPage++
+      }
+      let pageP=this.produits.slice(index,index+this.nbElements)
+      this.produits=pageP
+      console.log(this.totalPage);
+      
+    },(err)=>{
+      this.erreur=err.message
+      setTimeout(() => {
+        this.erreur=""
+      }, 16000);
+      console.log(err);
+      })
+
+  }
+
+  deleteData(p:any){
+    this.service.deleteProduct(p).subscribe((res)=>{
       console.log(res);
       this.reussite="Donnée supprimée avec succès !"
       setTimeout(() => {
         this.reussite=""
       }, 10000);
-      this.getProduits()
+      let id = this.produits.indexOf(p)
+      this.produits.splice(id,1)
       
     })
   }
---
+++ src/app/service/produits.service.ts
@@ -0,0 +1,11 @@
+
+export interface ProduitsService {
+
+  id:number
+  nom:string
+  prix:number
+  qte:number
+  promotion:string
+  
+
+}
